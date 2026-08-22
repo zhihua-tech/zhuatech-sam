27ead33cce418124c4baaa353c2422a9fa7da2fd
nero <233401861@qq.com>
2026-08-22 10:31:27 +0800

    chore: add website to copyright headers

--- backend/src/main/java/cn/zhuatech/sam/controller/LicensePositionController.java
+++ backend/src/main/java/cn/zhuatech/sam/controller/LicensePositionController.java
@@ -1,4 +1,4 @@
-/* Copyright 2026 上海如静知华信息科技有限公司 */
+/* Copyright 2026 上海如静知华信息科技有限公司 · https://www.zhuatech.cn/ */
 package cn.zhuatech.sam.controller;
 import cn.zhuatech.sam.common.ApiResponse; import cn.zhuatech.sam.service.LicensePositionService; import jakarta.validation.Valid; import org.springframework.web.bind.annotation.*;
 @RestController @RequestMapping("/api/admin/license-position") public class LicensePositionController {
